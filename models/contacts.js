02b83acf92e2534b4ab02d37855616a978b967c5
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-12-18 14:34:29 +0200

    end task

--- models/contacts.js
+++ models/contacts.js
@@ -1,22 +1,68 @@
 const fs = require('fs/promises');
 const path = require('path');
+const { nanoid } = require('nanoid');
 
-const contactsPath = path.join(__dirname,'./contacts.json')
-
+const contactsList = path.join(__dirname, './contacts.json');
 
 const listContacts = async () => {
-  const result = await fs.readFile(contactsPath);
-  
+  const result = await fs.readFile(contactsList);
   return JSON.parse(result);
 }
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId);
+  
+  return result;
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId)
+  if (index === -1) {
+    return null;
+  }
+  contacts.splice(index, 1);
+  await fs.writeFile(contactsList, JSON.stringify(contacts, null, 2));
+}
 
-const addContact = async (body) => {}
+const addContact = async ({name, email, phone}) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(5),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsList, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  let filteredContact = null;
+  const putContact = {
+    id: contactId,
+    name: body.name,
+    email: body.email,
+    phone: body.phone
+  };
+
+  contacts.forEach(contact => {
+    if (contact.id === contactId) {
+      contact.name = body.name;
+      contact.email = body.email;
+      contact.phone = body.phone;
+      filteredContact = contact.id;
+    };
+  });
+  
+  if (filteredContact !== null) {
+    return putContact;
+  };
+  return filteredContact;
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,30 +1,41 @@
-const express = require('express')
+const express = require('express');
+const contacts = require("../../models/contacts");
 
-const contacts = require("../../models/contacts")
-
-const router = express.Router()
-  
+const router = express.Router();
+const { postValidation, putValidation } = require('../../middlwares/validationMiddleware');
 
 router.get('/', async (req, res, next) => {
-  const contactList = await contacts.listContacts()
-  
-  res.json(contactList)
+  const contactList = await contacts.listContacts();  
+  res.json(contactList);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const oneContact = await contacts.getContactById(req.params.contactId);
+  if (oneContact.error) {
+    res.status(404).json({ message: "Not found" });
+  }
+  res.json(oneContact);
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', postValidation, async (req, res, next) => {
+  const postContact = await contacts.addContact(req.body);
+  res.status(201).json(postContact);
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const deleteContact = await contacts.removeContact(req.params.contactId)
+  if (deleteContact === null) {
+    return res.status(404).json({ message: "Not found" });
+  };
+  res.json({ message: "contact deleted" });
 })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', putValidation, async (req, res, next) => {
+  const updateContact = await contacts.updateContact(req.params.contactId, req.body);
+  if (updateContact === null) {
+    return res.status(404).json({ message: "Not found" });
+  };
+  res.json(updateContact);
 })
 
 module.exports = router
